f89b9f87e30bfc5fa2f75cdd10efd4d2ac5ff60e
whyttaker <whyttaker@gmail.com>
2023-03-06 19:46:52 -0800

    fixed drift mechanics, still only works on toyota

--- Assets/ToyotaStats.cs
+++ Assets/ToyotaStats.cs
@@ -7,7 +7,7 @@ public class ToyotaStats : CarMovement
     private Vector3 MoveForce;
     public float steerAngle = 10;
     public float Drag = 0.98f;
-    private float realAcceleration = 18f;
+    private float realAcceleration = 180f;
     private float traction = 0.5f;
 
     public ToyotaStats()
@@ -18,17 +18,15 @@ public class ToyotaStats : CarMovement
     }
 
     public void FixedUpdate(){
-        if(Input.GetButton("Fire1")){
+        if(Input.GetButton("Fire1") && Input.GetButton("Horizontal")){
             m_isDrifting = true;
         }
         else
             m_isDrifting = false;
         
 
-        if(!(m_isDrifting)){
-            MoveForce += transform.forward * realAcceleration * Input.GetAxis("Vertical");
+            MoveForce += transform.forward * realAcceleration * Input.GetAxis("Vertical")* Time.deltaTime;
             transform.position += MoveForce * Time.deltaTime;
-        }
 
         float steerInput = Input.GetAxis("Horizontal");
         transform.Rotate((Vector3.up * steerInput * MoveForce.magnitude * steerAngle * Time.deltaTime)/32);
@@ -36,8 +34,13 @@ public class ToyotaStats : CarMovement
         MoveForce *= Drag;
         MoveForce = Vector3.ClampMagnitude(MoveForce, 130);
 
-        if(m_isDrifting)
+        if (m_isDrifting) {
             MoveForce = Vector3.Lerp(MoveForce.normalized, transform.forward, traction * Time.deltaTime) * MoveForce.magnitude;
+        } else {
+            MoveForce = transform.forward * MoveForce.magnitude;
+        }
+        m_Rigidbody.AddForce(Vector3.down * 9.81f * 10, ForceMode.Acceleration);
     }
+
 
 }
