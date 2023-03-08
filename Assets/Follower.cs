edc76106207974601298dfff1dec6c9c6dcf6396
whyttaker <whyttaker@gmail.com>
2023-03-08 01:55:33 -0800

    improved the AI (not really)

--- Assets/Follower.cs
+++ Assets/Follower.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 using PathCreation;
 
 public class Follower : MonoBehaviour{
-     public PathCreator pathCreator;
-     public float speed = 5;
+    public PathCreator pathCreator;
+
+    public GameObject obj;
+    private float speed = 90;
+
+    
      float distanceTravelled;
      void Update() {
         distanceTravelled += speed * Time.deltaTime;
         transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled);
         transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled);
-
+        var distanceBetweenObjects = Vector3.Distance(transform.position, obj.transform.position);
+        if(distanceBetweenObjects > 50){
+            speed = 180;
+        }else if(distanceBetweenObjects > 100){
+            speed = 90;
+        }else{
+            speed = 140;
+        }
+        Debug.Log(speed);
     }
 }
