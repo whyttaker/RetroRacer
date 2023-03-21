c05cf86936c637ba41690c46dedb1783f0f97410
OceanaLee <lee.oceana001@gmail.com>
2023-03-20 23:44:25 -0700

    Added UI for the 1P mode for the power up effects
    added the CarPowUI script that changes around the UI accordingly when different power ups are picked up

---
+++ Assets/Scripts/Car/CarPowUI.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CarPowUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public CarMovement playerMovement;
+    public TextMeshProUGUI speedText;
+    public TextMeshProUGUI invertedText;
+    public TextMeshProUGUI boostMultText;
+
+    public void Awake()
+    {
+        playerMovement = GetComponent<CarMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(speedText)
+        { //if has a text box associated w the speed
+            speedText.text = playerMovement.m_Speed.ToString();
+        }
+
+        if (invertedText)
+        { //if has text box associated w inverted controls
+            if (playerMovement.m_Inverted)
+            {
+                Debug.Log("Inverted");
+                invertedText.text = "! INVERTED !";
+            }
+            else
+            {
+                invertedText.text = "";
+            }
+        }
+
+        if (boostMultText)
+        { // if has a text box associated w the boost multiplier
+            if (playerMovement.m_BoostMult > 1f)
+            {
+                Debug.Log("Boosting: " + playerMovement.m_BoostMult.ToString());
+                boostMultText.text = "2x Boost";
+            }
+            else
+            {
+                boostMultText.text = "";
+            }
+        }
+    }
+}
